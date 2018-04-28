c8b32eef0029efc3570b95576da8dcd7a46556ad
Julian Popescu <jpopesculian@gmail.com>
2018-04-28 21:25:14 +0200

    code splitting

--- pkg/api/update.go
+++ pkg/api/update.go
@@ -1,16 +1,14 @@
 package api
 
 import (
-	// "github.com/davecgh/go-spew/spew"
+	"github.com/davecgh/go-spew/spew"
 	"github.com/jpopesculian/papercli/pkg/config"
 	"github.com/jpopesculian/papercli/pkg/files"
 	"github.com/jpopesculian/papercli/pkg/store"
 	"log"
 )
 
-func Update(options *config.CliOptions) {
-	db := store.NewStore(options)
-	defer db.Close()
+func saveUpstreamDocuments(db *store.Store, options *config.CliOptions) (int, chan *store.Document, chan *files.DocumentNode, chan error) {
 	documents, cont := db.UpstreamDocuments()
 	count := 0
 	savedDocuments := make(chan *store.Document)
@@ -31,17 +29,24 @@ func Update(options *config.CliOptions) {
 		}(<-documents)
 		count++
 	}
+	return count, savedDocuments, savedTrees, errors
+}
+
+func Update(options *config.CliOptions) {
+	db := store.NewStore(options)
+	defer db.Close()
+	count, documents, trees, errors := saveUpstreamDocuments(db, options)
 	folderList := newUniqueFolderList(0)
 	for i := 0; i < count; i++ {
 		err := <-errors
 		if err != nil {
 			log.Println(err)
 		}
-		document := <-savedDocuments
+		document := <-documents
 		if document != nil {
 			db.SaveLocalDocument(document)
 		}
-		tree := <-savedTrees
+		tree := <-trees
 		if tree != nil {
 			folderList.add(files.TreeToFolderList(tree))
 		}
